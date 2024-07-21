a9b6796846a80291c113a4c11d8d8fde0c90da71
Ayusht777 <talesarayush@gmail.com>
2024-07-21 11:22:07 +0530

    performance fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
-
+import { lazy, Suspense } from 'react';
 import logo from "@/assets/favicon.png";
 import ExportButton from "@/components/Controller/ExportButton";
 import { ExportButtonProps } from "@/components/Controller/ExportButton";
 
-import AboutUsButton from "@/components/About/AboutUsButton";
+const AboutUsButton = lazy(() => import("@/components/About/AboutUsButton"));
 
 const Navbar = ({ targetRef }: ExportButtonProps) => {
+  const isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
+
   return (
     <nav className="flex w-full items-center justify-end bg-neutral-900 py-2 pl-4 pr-5">
       <div className="">
         <img src={logo} alt="" />
       </div>
       <div className="flex w-full items-center justify-end gap-x-2">
-        <AboutUsButton />
-        
+        {!isMobile && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <AboutUsButton />
+          </Suspense>
+        )}
         <ExportButton targetRef={targetRef} />
       </div>
     </nav>
